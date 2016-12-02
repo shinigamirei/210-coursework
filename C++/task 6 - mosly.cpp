160cb9a2d753886054296001d9b3ec9c5fc86e9b
shinigamirei <shinigamireyas@googlemail.com>
2016-12-02 16:01:04 +0000

    finished task 6

--- C++/task 6 - mosly.cpp
+++ C++/task 6 - mosly.cpp
@@ -2,10 +2,10 @@
 
 
 
-int mains(char* args, int argc)
+int main(char* args, int argc)
 {
 	char string[16] = {"This is awesome"};
-	char rString[16];
+	char rString[16] = { "               " };
 	int loopcount = 0;
 	int wordStart = 0;
 	int wordEnd;
@@ -13,22 +13,23 @@ int mains(char* args, int argc)
 	int rStrPoi = len;
 
 	//std::cout << string << len;
-	for (int i = 0; i < 16; i++)
+	for (int i = 0; i < len +1; i++)
 	{
-
-		if (string[i] == ' ' || i == len)
+		if (string[i] == ' ' || i == len )
 		{
-			wordEnd = i;	
-			for (int z = wordEnd; z > wordStart; z--)
+			wordEnd = i - 1;
+			rStrPoi = (len - wordStart);
+			for (int z = wordEnd; z > wordStart - 1; z--)
 			{
-				rStrPoi = (len - i);
 				rString[rStrPoi] = string[z];
+				rStrPoi = rStrPoi - 1;
 			}
 			wordStart = i+1;
 		}
 
 	}
-	std::cout << rString;
+	for (int j=0; j < len +1;j++)
+	std::cout << rString[j];
 
 	while (true) {}
 	return 0;
